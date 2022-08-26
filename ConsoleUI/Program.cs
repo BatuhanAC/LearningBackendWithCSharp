a7407171e5b36920a1a477de8f2c403e6405fd6e
Batuhan Acar <batuhan.acar1@outlook.com>
2022-08-26 15:52:34 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+//CustomerManagerTest();
+
+//ProductDtoTest();
+
+static void CustomerManagerTest()
+{
+    CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+    foreach (var customer in customerManager.GetAll())
+    {
+        Console.WriteLine(customer.ContactName);
+    }
+}
+
+static void ProductDtoTest()
+{
+    ProductManager testProductDto = new ProductManager(new EfProductDal());
+
+    foreach (var detail in testProductDto.GetProductDetails())
+    {
+        Console.WriteLine("{0}/{1}/{2}", detail.CategoryName, detail.ProductName, detail.UnitsInStock);
+    }
+}
