e55c73fbfe4bcad4c254555d8393b71df893422b
BatuhanAC <70262961+BatuhanAC@users.noreply.github.com>
2022-09-06 16:49:32 +0300

    Ders-12
    FluentValidation öğrendik ve AOP giriş yaptık.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using Entities.DTOs;
 using Core.Utilities.Results;
 using Business.Constants;
+using FluentValidation;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Aspects.Autofac.Validation;
 
 namespace Business.Concrete
 {
@@ -21,12 +25,10 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof (ProductValidator))]
         public IResult ADD(Product product)
         {
-            if (product.ProductName.Length <= 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //business codes
             _productDal.Add(product);
             return new SuccessResult( Messages.ProductAdded);
         }
